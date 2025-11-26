45a33fd9ff8cb962f87116b76a54b6b097158e47
Anggi <dwianggiana412@gmail.com>
2025-11-26 16:09:19 +0700

    menambah jsonwebtoken

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const db = require('./db.js');  
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { authenticateToken, authorizeRole } = require('./middleware/auth.js');
 
 const app = express();
